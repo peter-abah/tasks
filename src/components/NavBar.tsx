21e96bd8e67f8082470de159f75ccc90d83fe86e
Peter Abah <peterabah.ob@gmail.com>
2022-01-14 22:00:43 +0100

    Add button to show todo form to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,13 @@
+import { useAppDispatch } from "../app/hooks";
+import { updateTodoFormHidden as toggleHidden } from "../features/ui/uiSlice";
+
 const NavBar = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <nav className="py-4 px-3">
       <h1>Todo</h1>
+      <button onClick={() => dispatch(toggleHidden(false))}>Add Todo</button>
     </nav>
   );
 };
