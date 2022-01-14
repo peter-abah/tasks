96d7f098affc3e562722623b70ed75b1694ebb0a
Peter Abah <peterabah.ob@gmail.com>
2022-01-14 17:56:01 +0100

    Add projectsSlice for redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import todosReducer from '../features/todos/todosSlice';
+import projectsReducer from '../features/projects/projectsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     todos: todosReducer,
+    projects: projectsReducer,
   },
 });
 
---
+++ src/features/projects/projectsSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface Project {
+  title: string,
+  id: string,
+};
+
+export type ProjectsState = Project[];
+
+const initialState: ProjectsState = [];
+
+const projectsSlice = createSlice({
+  name: 'projects',
+  initialState,
+  reducers: {
+    // Adds a new project or updates an existing project
+    update: (state, action: PayloadAction<Project>) => {
+      const project = action.payload;
+
+      // filters project from state if its exists (existing project)
+      const filtered = state.filter(({ id }) => id !== project.id);
+      return [...filtered, project];
+    },
+
+    remove: (state, action: PayloadAction<string>) => {
+      const id = action.payload;
+      return state.filter((project) => project.id !== id);
+    },
+  }
+});
+
+export const { update, remove } = projectsSlice.actions;
+
+export default projectsSlice.reducer;
