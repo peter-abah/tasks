933718885fffd9b15f2eb3a9eccb45d4e521b453
Peter Abah <peterabah.ob@gmail.com>
2022-01-16 19:14:09 +0100

    Add styles for todo form modal

--- src/components/TodoFormModal.tsx
+++ src/components/TodoFormModal.tsx
@@ -8,13 +8,25 @@ interface Props {
   dueDate: string;
   priority: string;
   projectId: string;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  handleChange: (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => void;
   handleSubmit: (e: React.FormEvent) => void;
   closeModal: () => void;
 }
 
 const TodoFormModal = (props: Props) => {
   const projects = useAppSelector(selectProjects);
+
+  const projectOptions = () => {
+    return projects.map(({ id, title }) => (
+      <option key={id} value={id}>
+        {title}
+      </option>
+    ));
+  };
 
   const {
     title,
@@ -28,57 +40,96 @@ const TodoFormModal = (props: Props) => {
   } = props;
 
   return (
-    <div>
-      <form>
-        <div>
-          <label htmlFor="todo-title">Title</label>
+    <div className="bg-transparent fixed top-0 left-0 h-screen w-screen z-30 flex items-center justify-center">
+      <form className="w-4/5 max-w-lg bg-primary shadow-lg rounded-2xl overflow-hidden text-sm">
+        <div className="p-4 flex flex-col border-b border-neutral-700">
           <input
+            className="mb-2 focus-visible:outline-none"
             id="todo-title"
             type="text"
             name="title"
+            placeholder="e.g Clean the kitchen"
             value={title}
             onChange={handleChange}
           />
-        </div>
 
-        <div>
-          <label htmlFor="todo-description">Description</label>
           <textarea
+            className="h-20 focus-visible:outline-none"
             id="todo-description"
             name="description"
             value={description}
+            placeholder="Description"
             onChange={handleChange}
           ></textarea>
         </div>
 
-        <div>
-          <label htmlFor="todo-date">Due Date</label>
-          <input
-            id="todo-date"
-            type="date"
-            name="dueDate"
-            value={dueDate}
-            onChange={handleChange}
-          />
-        </div>
+        <div className="p-4">
+          <div className="pb-4 flex flex-wrap gap-4 justify-between items-center">
+            <div>
+              <label className="pr-2" htmlFor="todo-date">
+                Due Date:
+              </label>
+              <input
+                className="p-1 border rounded-md"
+                id="todo-date"
+                type="date"
+                name="dueDate"
+                value={dueDate}
+                onChange={handleChange}
+              />
+            </div>
 
-        <div>
-          <label htmlFor="todo-priority">Priority</label>
-          <input
-            id="todo-priority"
-            type="text"
-            name="priority"
-            value={priority}
-            onChange={handleChange}
-          />
-        </div>
+            <div>
+              <label className="pr-2" htmlFor="todo-priority">
+                Priority:
+              </label>
+              <select
+                className="appearance-none text-center focus-visible:outline-none p1 border rounded-md"
+                name="priority"
+                id="todo-priority"
+                onChange={handleChange}
+                value={priority}
+              >
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
 
-        <button type="submit" onClick={handleSubmit}>
-          Save
-        </button>
-        <button type="button" onClick={closeModal}>
-          Cancel
-        </button>
+            <div>
+              <label className="pr-2" htmlFor="todo-project">
+                Project
+              </label>
+
+              <select
+                className="appearance-none text-center focus-visible:outline-none p1 border rounded-md"
+                name="projectId"
+                id="todo-project"
+                onChange={handleChange}
+                value={projectId}
+              >
+                {projectOptions()}
+              </select>
+            </div>
+          </div>
+
+          <div className="flex">
+            <button
+              className="mr-4 px-4 py-1 rounded-md bg-amber-700"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Add
+            </button>
+            <button
+              className="px-4 py-1 rounded-md bg-red-700"
+              type="button"
+              onClick={closeModal}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
