d919c5fd60a142218eb09bd7262c2b02563554a9
Peter Abah <peterabah.ob@gmail.com>
2022-01-19 12:29:16 +0100

    Remove counter from redux state

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import tasksReducer from '../features/tasks/tasksSlice';
 import projectsReducer from '../features/projects/projectsSlice';
 import uiReducer from '../features/ui/uiSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     tasks: tasksReducer,
     projects: projectsReducer,
     ui: uiReducer,
