9a0f869a197a83b3ce93a3bbd72c6a1861cf6d7d
Peter Abah <peterabah.ob@gmail.com>
2022-01-20 16:29:04 +0100

    Add firebase config

---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0hdRxmXsCUkX_q9etTWa-MV77tjeMTLs",
+  authDomain: "todo-5ab00.firebaseapp.com",
+  projectId: "todo-5ab00",
+  storageBucket: "todo-5ab00.appspot.com",
+  messagingSenderId: "1052227958540",
+  appId: "1:1052227958540:web:e5759b4ac88d46554cbb29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
