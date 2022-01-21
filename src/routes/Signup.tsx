ce518fd376c46a3a597ab197e43c6211f46818fd
Peter Abah <peterabah.ob@gmail.com>
2022-01-21 19:05:39 +0100

    Refactor Signup route to use FormField component

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,5 +1,6 @@
-import { useFormik } from "formik";
+import { Formik, Form } from "formik";
 import { useNavigate } from "react-router-dom";
+import FormField from "../components/FormField";
 import { useAppDispatch } from "../app/hooks";
 import * as Yup from "yup";
 
@@ -15,102 +16,67 @@ const Signup = () => {
 
   const handleSuccess = (user: Iuser) => {
     dispatch(loginUser(user));
-    navigate('/');
+    navigate("/");
   };
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      password: "",
-      passwordConfirmation: "",
-    },
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
-      email: Yup.string()
-        .matches(emailRegex, "Invalid email address")
-        .required("Required"),
-      password: Yup.string()
-        .required("Required")
-        .min(8, "Password is too short - should be 8 characters minimum"),
-      passwordConfirmation: Yup.string().oneOf(
-        [Yup.ref("password"), null],
-        "Passwords must match"
-      ),
-    }),
-    onSubmit: ({ name, email, password }, { setFieldError }) => {
-      signupUser(name, email, password)
-        .then(handleSuccess)
-        .catch((e) => handleErrors(e, setFieldError));
-    },
-  });
-
   return (
-    <div>
-      <h1>Sign up</h1>
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        email: Yup.string()
+          .matches(emailRegex, "Invalid email address")
+          .required("Required"),
+        password: Yup.string()
+          .required("Required")
+          .min(8, "Password is too short - should be 8 characters minimum"),
+        passwordConfirmation: Yup.string().oneOf(
+          [Yup.ref("password"), null],
+          "Passwords must match"
+        ),
+      })}
+      onSubmit={({ name, email, password }, { setFieldError }) => {
+        signupUser(name, email, password)
+          .then(handleSuccess)
+          .catch((e) => handleErrors(e, setFieldError));
+      }}
+    >
+      <div className="flex flex-col w-screen max-w-xs mx-auto my-16 p-4 shadow-lg rounded-xl">
+        <h1 className="text-2xl font-bold pb-4">Sign up</h1>
+        <Form>
+          <FormField
+            label="Name:"
             name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            type="text"
+            placeholder="Peter"
           />
-          {formik.touched.name && formik.errors.name ? (
-            <p>{formik.errors.name}</p>
-          ) : null}
-        </div>
 
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
+          <FormField
+            label="Email:"
             name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            type="email"
+            placeholder="peter@email.com"
           />
-          {formik.touched.email && formik.errors.email ? (
-            <p>{formik.errors.email}</p>
-          ) : null}
-        </div>
 
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.password && formik.errors.password ? (
-            <p>{formik.errors.password}</p>
-          ) : null}
-        </div>
+          <FormField label="Password:" name="password" type="password" />
 
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
+          <FormField
+            label="Confirm Password:"
             name="passwordConfirmation"
-            value={formik.values.passwordConfirmation}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            type="password"
           />
-          {formik.touched.passwordConfirmation &&
-          formik.errors.passwordConfirmation ? (
-            <p>{formik.errors.passwordConfirmation}</p>
-          ) : null}
-        </div>
 
-        <button type="submit">Sign up</button>
-      </form>
-    </div>
+          <button className="block px-3 py-1 bg-emerald-700 rounded-lg mx-auto mt-4 text-lg shadow-md" type="submit">Sign up</button>
+        </Form>
+      </div>
+    </Formik>
   );
 };
 
