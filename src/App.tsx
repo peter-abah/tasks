a21018038c6ef8a82d29c244bf90b80fcb2af2ec
Peter Abah <peterabah.ob@gmail.com>
2022-01-21 21:37:55 +0100

    Add redirect to signin route if user not signed in

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import { Routes, Route } from 'react-router-dom';
-import { Layout, Project, Category, Signup, Signin } from './routes';
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { Layout, Project, Category, Signup, Signin } from "./routes";
+
+import { useAppSelector} from './app/hooks';
+import { selectUser } from './features/users/usersSlice';
 
 function App() {
+  const navigate = useNavigate();
+  const user = useAppSelector(selectUser);
+
+  useEffect(() => {
+    !user && navigate('/signin');
+  });
+  
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -10,8 +20,8 @@ function App() {
         <Route path="/:category" element={<Category />} />
         <Route index element={<Category />} />
       </Route>
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/signin' element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
     </Routes>
   );
 }
