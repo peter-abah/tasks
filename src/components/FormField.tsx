6a4a47e0e80612944273c8f7f460e51ae49e7e04
Peter Abah <peterabah.ob@gmail.com>
2022-01-21 19:05:14 +0100

    Add FormField component

---
+++ src/components/FormField.tsx
@@ -0,0 +1,34 @@
+import { useField } from "formik";
+
+interface Iprops {
+  label: string;
+  id?: string;
+  name: string;
+  type: string;
+  placeholder?: string;
+}
+
+const FormField = ({ label, ...props }: Iprops) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <div className="flex flex-col py-2">
+      <label
+        htmlFor={props.id || props.name}
+        className="pb-2 text-sm font-bold"
+      >
+        {label}
+      </label>
+      <input
+        className="mb-1 border rounded-lg px-2 py-1 focus-visible:border-2 focus-visible:outline-none"
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <p className="text-xs text-red-500">{meta.error}</p>
+      ) : null}
+    </div>
+  );
+};
+
+export default FormField;
